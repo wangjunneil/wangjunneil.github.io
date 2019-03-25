2a98b7bbe2f72960a38558d27a1039d7c1166aa8
哲剑 <zhejian.wj@alibaba-inc.com>
2019-03-25 18:16:34 +0800

    fix

--- assets/main.js
+++ assets/main.js
@@ -243,7 +243,7 @@
     // 注册service-worker
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', function() {
-            navigator.serviceWorker.register('/sw.js').then(function(registration) {
+            navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(function(registration) {
                 // Registration was successful
                 console.log('ServiceWorker registration successful with scope: ', registration.scope);
             }, function(err) {

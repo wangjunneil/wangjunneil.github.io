443a1ad505bf8a8565454bccc2427681923116fb
哲剑 <zhejian.wj@alibaba-inc.com>
2019-03-25 15:38:19 +0800

    fix

--- service-worker.js
+++ service-worker.js
@@ -3,22 +3,40 @@ var CACHE_NAME = CACHE_VERSION + ":sw-cache::";
 var URL_TO_CACHE = [
     "/",
     "/offline.html",
-    "/manifest.json"
+    "/manifest.json",
+    "/assets/jquery-1.9.1.min.js",
+    "/assets/core.css",
+    "/assets/js/nav.js",
+    "/assets/js/zepto.min.js",
+    "/assets/header.png",
+    "/assets/offline.gif",
+    "/assets/search.png",
+    "/assets/main.js",
+    "/assets/hacker.png",
+    "/assets/5-13050G31J7.gif",
+    "/assets/clear.png",
+    "/assets/icons/icon-144x144.png",
+    "/assets/favicon.ico",
+    "/assets/clear.png"
 ];
 
 // --------------------------------------------------------------------
 
 // 安装事件
-self.addEventListener('install', e => {
-    console.log("WORKER: Install completed");
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(URL_TO_CACHE).then(function(cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
 });
 
 // 激活事件
-self.addEventListener('activate', e => {
+self.addEventListener('activate', function(event) {
     console.log('Service Worker Activate');
 });
 
 // 请求事件
-self.addEventListener('fetch', e => {
-    console.log(e.request.url);
+self.addEventListener('fetch', function(event) {
+    console.log(event.request.url);
 });

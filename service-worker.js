dc4389602649b1a0b17de181a1adfe256106b600
哲剑 <zhejian.wj@alibaba-inc.com>
2019-03-25 16:55:38 +0800

    fix

--- service-worker.js
+++ service-worker.js
@@ -20,54 +20,55 @@ var URL_TO_CACHE = [
     '/assets/clear.png'
 ];
 
-function onInstall(event) {
-    event.waitUntil(caches.open(CACHE_NAME).then(function(cache) {
+self.addEventListener('install', function(event) {
+  event.waitUntil(caches.open(CACHE_NAME).then(function(cache) {
+        console.log("SERVICE WORKER: Install cache.")
         return cache.addAll(URL_TO_CACHE);
       })
   );
-}
+});
 
-// function onActivate(e) {
-//     console.log("[Serviceworker]", "Activating!", e);
+self.addEventListener("activate", function(e) {
+    console.log("[Serviceworker]", "Activating!", e);
 
-//     e.waitUntil(caches.keys().then(keyList => {
-//         return Promise.all(keyList.map(key => {
-//             if (key != CACHE_NAME) {
-//                 console.log('[ServiceWorker] Removing old cache', key);
-//                 return caches.delete(key);
-//             }
-//         }));
-//     }));
-// }
+    e.waitUntil(caches.keys().then(keyList => {
+        return Promise.all(keyList.map(key => {
+            if (key != CACHE_NAME) {
+                console.log('[ServiceWorker] Removing old cache', key);
+                return caches.delete(key);
+            }
+        }));
+    }));
+});
 
-// function onFetch(e) {
-//     e.respondWith(
-//         caches.match(e.request).then(resp => {
-//             // 缓存命中直接返回
-//             if (resp) {
-//                 return resp;
-//             }
+self.addEventListener("fetch", function(e) {
+    e.respondWith(
+        caches.match(e.request).then(resp => {
+            // 缓存命中直接返回
+            if (resp) {
+                return resp;
+            }
 
-//             var fetchRequest = e.request.clone();
-//             return fetch(fetchRequest).then(response => {
-//                 // 检查是否收到无效的响应
-//                 if (!response || response.status !== 200 || response.type !== 'basic') {
-//                     return response || caches.match("/offline.html");
-//                 }
+            var fetchRequest = e.request.clone();
+            return fetch(fetchRequest).then(response => {
+                // 检查是否收到无效的响应
+                if (!response || response.status !== 200 || response.type !== 'basic') {
+                    return response || caches.match("/offline.html");
+                }
 
-//                 var responseToCache = response.clone();
-//                 caches.open(CACHE_NAME).then(cache => {
-//                     cache.put(e.request, responseToCache);
-//                 });
+                var responseToCache = response.clone();
+                caches.open(CACHE_NAME).then(cache => {
+                    cache.put(e.request, responseToCache);
+                });
 
-//                 return response;
-//             })
-//             .catch (err => {
-//                 return caches.match('/offline.html');
-//             });
-//         })
-//     )
-// }
+                return response;
+            })
+            .catch (err => {
+                return caches.match('/offline.html');
+            });
+        })
+    )
+});
 
 // // 接收推送消息
 // function onPush(event) {
@@ -116,7 +117,7 @@ function onInstall(event) {
 // }
 
 // // Service Worker 事件注册
-self.addEventListener("install", onInstall);
+// self.addEventListener("install", onInstall);
 // self.addEventListener("activate", onActivate),
 // self.addEventListener("fetch", onFetch),
 // self.addEventListener('push', onPush),

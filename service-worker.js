c7bb3ef17817e3e85f34f7652307793e00443178
哲剑 <zhejian.wj@alibaba-inc.com>
2019-03-25 16:10:23 +0800

    fix

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,35 @@
+var CACHE_VERSION = "V1.1.0";
+var CACHE_NAME = CACHE_VERSION + ":sw-cache::";
+var URL_TO_CACHE = [
+    '/',
+    '/offline.html',
+    '/manifest.json',
+    '/assets/jquery-1.9.1.min.js',
+    '/assets/core.css',
+    '/assets/js/nav.js',
+    '/assets/js/zepto.min.js'
+];
+
+// var URL_TO_CACHE = [
+//     '/',
+//     '/offline.html',
+//     '/manifest.json',
+//     '/assets/jquery-1.9.1.min.js',
+//     '/assets/core.css',
+//     '/assets/js/nav.js',
+//     '/assets/js/zepto.min.js',
+//     '/assets/header.png',
+//     '/assets/offline.gif',
+//     '/assets/search.png',
+//     '/assets/main.js',
+//     '/assets/hacker.png',
+//     '/assets/5-13050G31J7.gif',
+//     '/assets/clear.png',
+//     '/assets/icons/icon-144x144.png',
+//     '/assets/favicon.ico',
+//     '/assets/clear.png'
+// ];
+
 function onInstall(event) {
     event.waitUntil(
     caches.open(CACHE_NAME)
@@ -96,27 +128,6 @@ function onNotificationClick(event) {
     }
 }
 
-var CACHE_VERSION = "V1.1.0";
-var CACHE_NAME = CACHE_VERSION + ":sw-cache::";
-var URL_TO_CACHE = [
-    '/',
-    '/offline.html',
-    '/manifest.json',
-    '/assets/jquery-1.9.1.min.js',
-    '/assets/core.css',
-    '/assets/js/nav.js',
-    '/assets/js/zepto.min.js',
-    '/assets/header.png',
-    '/assets/offline.gif',
-    '/assets/search.png',
-    '/assets/main.js',
-    '/assets/hacker.png',
-    '/assets/5-13050G31J7.gif',
-    '/assets/clear.png',
-    '/assets/icons/icon-144x144.png',
-    '/assets/favicon.ico',
-    '/assets/clear.png'
-];
 // Service Worker 事件注册
 self.addEventListener("install", onInstall),
 self.addEventListener("activate", onActivate),

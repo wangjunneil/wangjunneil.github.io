73d2265f2358be561536c6207accf4a098111d3b
哲剑 <zhejian.wj@alibaba-inc.com>
2019-03-25 15:28:58 +0800

    fix

--- service-worker.js
+++ service-worker.js
@@ -3,21 +3,7 @@ var CACHE_NAME = CACHE_VERSION + ":sw-cache::";
 var URL_TO_CACHE = [
     "/",
     "/offline.html",
-    "/manifest.json",
-    "/assets/jquery-1.9.1.min.js",
-    "/assets/core.css",
-    "/assets/js/nav.js",
-    "/assets/js/zepto.min.js",
-    "/assets/header.png",
-    "/assets/offline.gif",
-    "/assets/search.png",
-    "/assets/main.js",
-    "/assets/hacker.png",
-    "/assets/5-13050G31J7.gif",
-    "/assets/clear.png",
-    "/assets/icons/icon-144x144.png",
-    "/assets/favicon.ico",
-    "/assets/clear.png"
+    "/manifest.json"
 ];
 
 // --------------------------------------------------------------------

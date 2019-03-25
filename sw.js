223f0cbf25ab85545df4f7e715886122d01142c7
哲剑 <zhejian.wj@alibaba-inc.com>
2019-03-25 18:31:56 +0800

    fix

--- sw.js
+++ sw.js
@@ -94,7 +94,7 @@ function onNotificationClick(event) {
     }
 }
 
-var CACHE_VERSION = "V1.1.0";
+var CACHE_VERSION = "V1.1.1";
 var CACHE_NAME = CACHE_VERSION + ":sw-cache::";
 var URL_TO_CACHE = [
     "/",
